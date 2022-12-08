b9d9be6456666a2017f21d90fa2e939b0782e503
knmcguire <kimberly@bitcraze.io>
2022-12-08 20:24:25 +0100

    haven't finished day 7 but will try again soon

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,7 +1,11 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::{File, remove_dir_all, remove_file, create_dir};
+use std::io::{BufRead, BufReader, Write};
 use std::collections::HashMap;
 use std::process::Command;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+
 
 fn main() {
     
@@ -9,28 +13,24 @@ fn main() {
     let mut file_str = File::open(&args[1]).unwrap();
     let mut reader = BufReader::new(file_str).lines();
 
-    let mut dir_map: HashMap<String, u32> = HashMap::new();
-    let mut dir_name = String::new();
-    let mut dir_size: u32 = 0;
+    let file_path = PathBuf::from("target/test_dir/output.txt");
+    create_dir("target/test_dir").unwrap();
+    let mut file = File::create(file_path).unwrap();
+    file.write_all(b"").unwrap();
 
     for line in reader {
         let line_str = line.unwrap();
-        let mut line_tmp = line_str.clone();
 
         if line_str.contains("$ ") {
 
-            println!(" {:?}", line_tmp);
+            println!(" {:?}", line_str);
         }else if line_str.contains("dir ") {
-            println!("{:?}", dir_name);
-            dir_map.insert(dir_name, dir_size);
-            dir_name = line_tmp.replace("dir ", "");
-            dir_size = 0;
-            println!("{:?}", dir_name);
-        }else {
-            let file_vec = line_tmp.split(" ").collect::<Vec<&str>>();
 
-            dir_size += file_vec[0].parse::<u32>().unwrap();
+            println!("{:?}", line_str);
+        }else {
+            println!("{:?}", line_str);
+
         }
     }
-    println!("{:?}", dir_map);
+    remove_dir_all("target/test_dir").unwrap();
 }
