f7985a2a4b58df86d5a4c7d71ec2ff57f89e6a92
knmcguire <kimberly@bitcraze.io>
2022-12-10 12:54:17 +0100

    works for longer exxample

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -7,20 +7,40 @@ fn main() {
     let file_str = File::open(&args[1]).unwrap();
     let reader = BufReader::new(file_str).lines();
 
-    let mut x : i32 = 1;
-    let mut cycle_num = 0;
+
+
     // Loop through the lines of the file and retrieve the string
+    let mut vec_str : Vec<String> = Vec :: new();
     for line in reader {
         let line_str = line.unwrap();
         if line_str.contains("noop") {
+            vec_str.push("noop".to_string());
+        } else {
+            vec_str.push("noop".to_string());
+            let mut instruction = line_str.clone();
+            vec_str.push(instruction);
+        }
+    }
+    let mut x : i32 = 1;
+    let mut cycle_num = 1;
+    let mut sum = 0;
+    for line in vec_str.iter() {
+        if line.contains("noop") {
             cycle_num += 1;
         } else {
             // retrieve last number of string including the sign
-            let number : i32 = line_str.split_whitespace().last().unwrap().parse().unwrap();
+            let number : i32 = line.split_whitespace().last().unwrap().parse().unwrap();
             x += number;
-            cycle_num+= 2;
+            cycle_num+= 1;
+        }
+        let array = [20, 60, 100, 140, 180, 220];
+        // check if cycle_num is in array
+        if array.contains(&cycle_num) {
+            println!("cycle_num: {}, x: {}, {}", cycle_num, x, cycle_num*x);
+            sum += cycle_num*x
         }
         println!("cycle_num: {}, x: {}", cycle_num, x);
     }
+    println!("sum: {}", sum);
 
 }
