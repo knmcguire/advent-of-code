2a6427be636d29ef80b9ca6f4890b1f30e85d117
knmcguire <kimberly@bitcraze.io>
2022-12-10 14:49:46 +0100

    part2 works!

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -20,9 +20,9 @@ fn main() {
         } else {
             let instruction = line_str.clone();
             vec_str.push("noop".to_string());
-            vec2_str.push(instruction.clone());
-            vec_str.push(instruction.clone());
             vec2_str.push("noop".to_string());
+            vec_str.push(instruction.clone());
+            vec2_str.push(instruction.clone());
         }
     }
     let mut x : i32 = 1;
@@ -61,40 +61,45 @@ fn main() {
 
     cycle_num = 1;
     let mut crt_level: usize = 0;
+    let mut position_sprite = 1;
     for line in vec2_str.iter() {
+        let current_index = ((cycle_num-1) % 40 ) as usize;
+        crt_level = ((cycle_num-1)  / 40 )as usize  ;
+
+        //if current index is similar or 1 off from position_sprite, than draw '#'
+        // or else draw '.'
+        let mut current_drawn_character = '.';
+        if current_index == position_sprite as usize || current_index == (position_sprite - 1) as usize || current_index == (position_sprite + 1) as usize {
+            current_drawn_character = '#';
+        }
+        CRT[crt_level][current_index] = current_drawn_character;
+        println!("current_index: {}, position_sprite: {}, current_drawn_character: {}", current_index, position_sprite, current_drawn_character);
+        
+        //print current level of crt
+        for line in CRT.iter() {
+            for character in line.iter() {
+                print!("{}", character);
+            }
+            println!("");
+        }
+
         if line.contains("noop") {
-            let current_index = ((cycle_num-1) % 40 ) as usize;
-            crt_level = ((cycle_num-1)  / 40 )as usize  ;
-            let current_drawn_character = sprite.chars().nth(current_index).unwrap();
-            CRT[crt_level][current_index] = current_drawn_character;
+
             cycle_num += 1;
         } else {
             
-
             // second cycle of addx
-            let current_index = ((cycle_num-1) % 40 ) as usize;
-            crt_level = ((cycle_num-1) / 40 ) as usize;
-            let current_drawn_character = sprite.chars().nth(current_index).unwrap();
-            CRT[crt_level][current_index] = current_drawn_character;
-
             let number : i32 = line.split_whitespace().last().unwrap().parse().unwrap();
-            // In sprite, find the location of the "###" and give position
-            let position = sprite.find("###").unwrap() + 1;
-            println!("position: {} x {}", position, number);
-            // remove the "###" from the sprite
-            sprite.replace_range(position-1..position+2, "...");
-            // add the "###" to the new position
-            if position as i32 + number < 0 {
-                sprite.replace_range(0..3, "###");
-            } else if position as i32 + number > 39 {
-                sprite.replace_range(37..40, "###");
-            } else {
-                sprite.replace_range((position as i32 + number - 1) as usize..(position as i32 + number + 2) as usize, "###");
-            }
+            position_sprite = position_sprite + number;
+            println!("position_sprite: {}", position_sprite);
+
+            // print out the sprite
+            println!("sprite: {}", sprite);
             cycle_num+= 1;
 
+
+
         }
-        //println!("cycle_num: {}, x: {}", cycle_num, x);
     }
     // print out the CRT
     for line in CRT.iter() {
