ebd623c832ef16f81a46e187c21eeea359b2d34a
knmcguire <kimberly@bitcraze.io>
2022-12-12 23:48:02 +0100

    fixed for example input

--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -48,88 +48,59 @@ fn main() {
     }
 
     let mut current_location = start;
+    let mut previous_location = start;
+    
     let mut steps = 0;
-    for it in 0..50{
+    // create an vector of ones of size 4 of (i8, i8)
+    let mut possible_moves: Vec<(i8, i8)> = vec![(1, 0),(0, 1),(-1, 0),(0, -1)];
+    while current_location != end {
         let mut current_char = map[current_location.0][current_location.1];
         let mut current_char_int = find_index_char_in_alphabet(current_char);
-        // Get coordinates of the next location either up, down, left or right
-        // Which get's the closest to the end
-        // Check the next location's char and convert it to an integer
-        // If the next location's integer at most one higher than the current location's integer
-        // Then move to the next location
+        let mut possible_positions: Vec<(usize, usize)> = Vec::new();
 
-        // Check up
-        // Check which location is closer to the end
-        let mut k : i8 = 0;
-        if current_location.0 > end.0 {
-            k = -1;
-        } else {
-            k = 1;
-        }
-        let mut m : i8 = 0;
-        if current_location.1 > end.1 {
-            m = -1;
-        } else {
-            m = 1;
-        } 
-
-        let mut vec_direction : Vec<(i8, i8)> = vec![(1, 0), (0, 1), (-1, 0), (0, -1)]; // up, right, down, left
-        let mut vec_direction_score : Vec<(usize)> = vec![0,0,0,0];
-        for p in 0..4 {
-            println!("p: {}", p);
-
-            let direction = vec_direction[p];
-            let mut new_location = (current_location.0 + direction.0 as usize, current_location.1 + direction.1 as usize);
-            // if new_location is outside of bounds of the map, then skip it
-            if new_location.0 >= map.len() || new_location.1 >= map[0].len() {
-                vec_direction_score.push(0);
+        // get list of possible moves, up down left right
+        for possible_move in &mut possible_moves {
+            let mut possible_next_position = (current_location.0+ possible_move.0 as usize, current_location.1 + possible_move.1 as usize);
+            
+            // check if next position is still in the map
+            if possible_next_position.0 >= map.len() || possible_next_position.1 >= map[0].len() {
                 continue;
             }
-            let mut up_char = map[new_location.0][new_location.1];
-            let mut up_char_int = find_index_char_in_alphabet(up_char);
-            if up_char_int <= current_char_int + 1 {
-                // Check which location is closer to the end
-                let mut score = 0;
-                if (new_location.0 >= map.len() || new_location.1 >= map[0].len()) && up_char_int <= current_char_int + 1 {
-
-                    if new_location.0 > end.0 {
-                        score += new_location.0 - end.0;
-                    } else {
-                        score += end.0 - new_location.0;
-                    }
-                    if new_location.1 > end.1 {
-                        score += new_location.1 - end.1;
-                    } else {
-                        score += end.1 - new_location.1;
-                    }
-                }
-                vec_direction_score.push(score);
-            } 
-        }
-        // find index of hightest score
-        let mut index = 0;
-        let mut highest_score = 0;
-        for (i, score) in vec_direction_score.iter().enumerate() {
-            if *score > highest_score {
-                highest_score = *score;
-                index = i;
+            println!(" possible_next_position:  {:?} previous_location {:?}", possible_next_position, previous_location);
+            if possible_next_position.0 == previous_location.0 && possible_next_position.1 == previous_location.1 {
+                println!("previous location");
+                continue;
+            }
+            let mut possible_next_char = map[possible_next_position.0][possible_next_position.1];
+            let mut possible_next_char_int = find_index_char_in_alphabet(possible_next_char);
+            if possible_next_char_int <= current_char_int + 1  {
+                possible_positions.push(possible_next_position);
             }
         }
-        println!("index: {}", index);
-        // move to the location with the highest score
-        current_location = (current_location.0 + vec_direction[index].0 as usize, current_location.1 + vec_direction[index].1 as usize);
 
-        println!("current_location: {:?}", current_location);
-
-        if current_location == end {
-            println!("break");
+        // From the possible_positions, prefer the one with the highest char value
+        let mut closest_position = (0, 0);
+        let mut closest_char = ' ';
+        let mut closest_char_int = 0;
+        for possible_position in &possible_positions {
+            let mut possible_char = map[possible_position.0][possible_position.1];
+            let mut possible_char_int = find_index_char_in_alphabet(possible_char);
+            if possible_char_int > closest_char_int {
+                closest_char_int = possible_char_int;
+                closest_char = possible_char;
+                closest_position = *possible_position;
+            }
+        }
+        previous_location = current_location.clone();
+        steps += 1;
+        current_location = (closest_position.0 as usize, closest_position.1 as usize);
+        println!("current location: {:?}", current_location);
+        if current_location.0 == end.0 && current_location.1 == end.1 {
+            println!("found end at {} steps", steps);
             break;
         }
 
-
     }
-
-
-
+        
 
 }
