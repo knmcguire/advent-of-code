746965b400ce24acb5bfe8b059fa4a964534fd5e
knmcguire <kimberly@bitcraze.io>
2022-12-12 21:52:29 +0100

    solved part2 with hints!

--- 2022/day11/src/main.rs
+++ 2022/day11/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs;
 
-
 // a struct that is meant to be deserialized from a yaml file of the format:
 // Monkey 0:
 //   Starting items: 79, 98
@@ -18,12 +17,44 @@ struct Monkey {
     inspection_count: u32,
 }
 
+fn is_prime_number(number: u32) -> bool {
+    if number == 1 {
+        return false;
+    }
+    if number == 2 {
+        return true;
+    }
+    if number % 2 == 0 {
+        return false;
+    }
+    let mut i = 3;
+    while i * i <= number {
+        if number % i == 0 {
+            return false;
+        }
+        i += 2;
+    }
+    true
+}
+
+// find least common denominator between two values
+fn find_lcm(a: u32, b: u32) -> u32 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
 
 fn main() {
     // Get file name from terminal argument input and read to string
     let filename = std::env::args().nth(1).expect("No file name given");
     let input = std::fs::read_to_string(filename).expect("File not found");
     let mut monkeys = Vec::new();
+    let mut divideable_number: u64 = 1;
     // Loop through string, stopping at each line containing "Monkey"
     for line in input.lines() {
         if line.contains("Monkey") {
@@ -55,6 +86,7 @@ fn main() {
                 }
                 if line2.contains("Test") {
                     monkey.test = line2.split(": divisible by ").skip(1).next().unwrap().trim().parse().unwrap();
+                    divideable_number *= monkey.test as u64;
                 }
                 if line2.contains("If true") {
                     monkey.if_true = line2.split(": throw to monkey").skip(1).next().unwrap().trim().parse().unwrap();
@@ -67,55 +99,79 @@ fn main() {
         }
     }
 
-    for round in 0..20 {
+
+    for round in 0..10000
+
+     {
         println!("Round {}", round);
-    //print the starting items vector of all monkeys in the vector
-    for monkey in &monkeys {
-        println!("Monkey {} has {:?} items", monkey.if_true, monkey.starting_items);
-    }
+        // loop through all monkeys in the vector and list their starting items
+
+
         for nr in 0..monkeys.len() {
+    
             // clone the monkey struct
-            let  monkey = monkeys[nr].clone();
+            let monkey = monkeys[nr].clone();
             let operation = monkey.operation.clone();
             let starting_items = monkey.starting_items.clone();
             let test = monkey.test.clone();
             let if_true = monkey.if_true.clone();
             let if_false = monkey.if_false.clone();
 
+
             for nr_item in 0..starting_items.len() {
                 // get the current index of the item in the list
                 let item = starting_items[nr_item].clone();
-                let mut value = item.clone();
+                let mut value = item.clone() as u64;
+                
                 // divide value by three and round of to the nearest integer
                 if operation.contains("new = old * old") {
                     value = value * value;
                 } else if operation.contains("new = old *") {
                     let multiplier: u32 = operation.split("new = old * ").skip(1).next().unwrap().trim().parse().unwrap();
-                    value = value * multiplier;
+                    value = value * (multiplier as u64);
                 }else if operation.contains("new = old +") {
                     let adder: u32 = operation.split("new = old +").skip(1).next().unwrap().trim().parse().unwrap();
-                    value = value + adder;
+                    value = value + adder as u64;
                 }
-                value = (value as f32 / 3.0) as u32;
 
-                if value % test == 0 {
+                // is value a prime number?
+                
+                value = value % divideable_number;
+
+
+                if value % (test as u64) == 0 {
                     //println!("True: Monkey {} throws {} with value {} to monkey {}", nr, item,value, monkey.if_true);
                     let new_monkey_value = if_true.clone() as usize;
                     let new_monkey = &mut monkeys[new_monkey_value];
-                    new_monkey.starting_items.push(value); 
+                    new_monkey.starting_items.push(value as u32); 
                 } else {
                     //println!("False: Monkey {} throws {} with value {} to monkey {}", nr, item,value, monkey.if_false);
                     let new_monkey_value = if_false.clone() as usize;
                     let new_monkey = &mut monkeys[new_monkey_value];
-                    new_monkey.starting_items.push(value);
+                    new_monkey.starting_items.push(value as u32);
                 }
                 monkeys[nr].inspection_count += 1;
+
             }
             // remove all items from the starting_items vector of the original monkey
             monkeys[nr].starting_items.clear();
+
         }
 
-}
+
+        // print all the inspection counts of all monkeys in the vector
+        let mut nr = 0;
+        for monkey in &monkeys {
+            println!("Monkey {} has inspected {} items", nr, monkey.inspection_count);
+            nr += 1;
+        }
+        
+
+
+    }
+
+
+    println!("ROUNDS OVER");
     //print the starting items vector of all monkeys in the vector
     for monkey in &monkeys {
         println!("Monkey {} has {:?} items", monkey.if_true, monkey.starting_items);
@@ -126,11 +182,12 @@ fn main() {
     }
 
     // List all the inspection counts in a vector and sort from higest to lowest
-    let mut inspection_counts = Vec::new();
+    let mut inspection_counts :Vec<u64> = Vec::new();
     for monkey in &monkeys {
-        inspection_counts.push(monkey.inspection_count);
+        inspection_counts.push(monkey.inspection_count as u64);
     }
     inspection_counts.sort();
     inspection_counts.reverse();
+    println!("The highest inspection count is {} and {}", inspection_counts[0], inspection_counts[1]);
     println!("Monkey business is is {}", inspection_counts[0]*inspection_counts[1]);
 }
