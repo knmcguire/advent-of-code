d1d9730d06d304caad1659f7094739c7d7547d59
knmcguire <kimberly@bitcraze.io>
2023-12-03 14:30:47 +0100

    day 1 part 2

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -10,15 +10,68 @@ fn part1(input: &str) {
         numbers = format!("{}{}", first, last);
 
         let numbers: i32 = numbers.parse().unwrap();
-        println!("{}", numbers);
+        //println!("{}", numbers);
         total_sum += numbers;
     }
-    println!("Total sum: {}", total_sum);
+    println!("Day1 Part 1 -- Total sum: {}", total_sum);
 }
+
+fn part2(input: &str) {
+    // initialize total sum
+    let mut total_sum: i32 = 0;
+    // Go through the string in a forloop for each line
+    for line in input.lines() {
+
+
+        // Get the number from the line by combining the first and last numeric character and convert it to a i32
+        let numbers = find_numbers(line);
+        // combine the first and last number as 2 characters
+        let first = numbers[0];
+        let last = numbers[numbers.len() - 1];
+        let numbers = format!("{}{}", first, last);
+        let numbers: i32 = numbers.parse().unwrap();
+        total_sum += numbers;
+    }
+    println!("Day1 Part 2 -- Total sum: {}", total_sum);
+}
+
+fn find_numbers(input: &str) -> Vec<i32> {
+    let numbers = [("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)];
+    let mut found_numbers = Vec::new();
+    let mut start = 0;
+    while start < input.len() {
+        if let Some(digit) = input[start..].chars().next().and_then(|c| c.to_digit(10)) {
+            found_numbers.push(digit as i32);
+        } else {
+            let mut found = false;
+            for (word, number) in &numbers {
+
+                if input[start..].starts_with(word) {
+
+
+                    found_numbers.push(*number);
+                    found = true;
+                    break;
+                }
+            }
+
+        }
+        start += 1;
+
+    }
+    found_numbers
+}
+
+
 
 fn main() {
     // Input strings from text file
     let input = std::fs::read_to_string("input1.txt").unwrap();
     // find out what type input to be input into a function
-    part1(&input);
+    //part1(&input);
+    part2(&input);
+    let input = "zoneight234";
+    let numbers = find_numbers(input);
+    println!("{:?}", numbers);
+    //print vector
 }
