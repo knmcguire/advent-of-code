f0364d3fa374a8167abf5d4b60473c64d5b7fd1f
knmcguire <kimberly@bitcraze.io>
2023-12-04 16:30:28 +0100

    found right answer on input

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -1,5 +1,5 @@
-fn main() {
-    let input = std::fs::read_to_string("example1.txt").unwrap();
+fn part1(input: &str)
+{
     // parse input into a 2n vector, rows corresponding to the color
     // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     // Game 2: 4 red, 5 blue; 1 red, 2 green, 6 blue; 2 green
@@ -14,53 +14,58 @@ fn main() {
         let numbers = line.to_string();
         // split strings with : or ;
         let numbers: Vec<&str> = numbers.split(|c| c == ':' || c == ';').collect();
-        println!("{:?}", numbers);
-        for i in 0..numbers.len() {
+
+        for i in 1..numbers.len() {
             //split up numbers by ,
             let color: Vec<&str> = numbers[i].split(",").collect();
+
 
             // if string contains "blue"
             for n in 0..color.len() {
                 if color[n].contains("blue") {
-                    // split string with "blue
                     let color: Vec<&str> = color[n].split("blue").collect();
-                    println!("{:?}", color);
-                    // get the first element of the vector and convert it to an integer
                     let blue = color[0].trim().parse::<i32>().unwrap();
-                    println!("{:?}", blue);
-                    if blue > 12 {
+                    if blue == 14 {
+                        println!("blue is 14");
+                    }
+                    if 14 < blue {
+                        println!("too many blue");
                         possible = false;
                     }
                 }
                 else if color[n].contains("red") {
                     let color: Vec<&str> = color[n].split("red").collect();
-                    println!("{:?}", color);
                     let red = color[0].trim().parse::<i32>().unwrap();
-                    println!("{:?}", red);
-                    if red > 13 {
+                    if 12 < red {
+                        println!("too many red");
                         possible = false;
                     }
                 }
                 else if color[n].contains("green") {
                     let color: Vec<&str> = color[n].split("green").collect();
-                    println!("{:?}", color);
                     let green = color[0].trim().parse::<i32>().unwrap();
-                    println!("{:?}", green);
-                    if green > 14 {
+                    if 13 < green {
+                        println!("too many green");
                         possible = false;
                     }
                 } else {
                     println!("Error");
                 }
-                println!("{:?}", possible);
             }
-
-            let number = numbers[i].to_string();
-
         }
+
+        println!("{:?} {}", possible, game_number);
+
         if possible {
             total_possible_ids += game_number;
         }
+        println!("total possible ids {:?}", total_possible_ids);
     }
-println ! ("{:?}", total_possible_ids);
+    println!("Day2 Part 1 -- Total sum of ids: {}", total_possible_ids);
+}
+fn main() {
+    // Input strings from text file
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    // find out what type input to be input into a function
+    part1(&input);
 }
