0a4048f268d1c35604ff842fbf9d1fbbec0a05eb
knmcguire <kimberly@bitcraze.io>
2023-12-04 16:31:02 +0100

    clean up

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -19,17 +19,14 @@ fn part1(input: &str)
             //split up numbers by ,
             let color: Vec<&str> = numbers[i].split(",").collect();
 
-
             // if string contains "blue"
             for n in 0..color.len() {
                 if color[n].contains("blue") {
                     let color: Vec<&str> = color[n].split("blue").collect();
                     let blue = color[0].trim().parse::<i32>().unwrap();
                     if blue == 14 {
-                        println!("blue is 14");
                     }
                     if 14 < blue {
-                        println!("too many blue");
                         possible = false;
                     }
                 }
@@ -37,7 +34,6 @@ fn part1(input: &str)
                     let color: Vec<&str> = color[n].split("red").collect();
                     let red = color[0].trim().parse::<i32>().unwrap();
                     if 12 < red {
-                        println!("too many red");
                         possible = false;
                     }
                 }
@@ -45,21 +41,17 @@ fn part1(input: &str)
                     let color: Vec<&str> = color[n].split("green").collect();
                     let green = color[0].trim().parse::<i32>().unwrap();
                     if 13 < green {
-                        println!("too many green");
                         possible = false;
                     }
                 } else {
-                    println!("Error");
                 }
             }
         }
 
-        println!("{:?} {}", possible, game_number);
 
         if possible {
             total_possible_ids += game_number;
         }
-        println!("total possible ids {:?}", total_possible_ids);
     }
     println!("Day2 Part 1 -- Total sum of ids: {}", total_possible_ids);
 }
